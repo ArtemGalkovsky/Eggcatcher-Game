9830a710507d64e2b8ab48a58ce09b30faedab67
ArtemGalkovsky <art_galkovsky@mail.ru>
2020-10-11 17:39:49 +0300

    Update web

---
+++ web/JS/app.js
@@ -0,0 +1,164 @@
+const canvas = document.querySelector(".canvas")
+
+const c = canvas.getContext("2d")
+
+const w = canvas.width = canvas.offsetWidth
+const h = canvas.height = canvas.offsetHeight
+
+function drawBg () {
+	c.fillStyle = "#43cffa"
+	c.fillRect(0, 0, 700, 500)
+
+	c.fillStyle = "#15b046"
+	c.fillRect(0, 500, 700, 200)
+}
+
+
+function createEgg (x, y, color, w, h) {
+	
+	c.beginPath()
+	//c.moveTo(x, y)
+	c.fillStyle = `rgb(${color.join(", ")})`//{Array.join(color)}`
+	c.ellipse(x, y, w, h, 0, 0, Math.PI * 2)
+	c.fill()
+	//c.bezierCurveTo(x + 173, y + 79, x + 136, y + 436, x + 18, y + 407)
+	// c.stroke()
+	//c.bezierCurveTo(x + 0, y + 18, x + 19, y + 101, x, y)//(x + 60, y + 90, x + 40, y + 50, x, y)
+	// c.bezierCurveTo(89 + x, 251 + y, 232 + x, 330 + y, 232 + x, 315 + y);
+	// c.stroke()
+	return [x, y, color]
+}
+
+// var EggsList = []
+
+function createBasket (x, y, w, h) {
+	c.lineWidth = 10
+	c.strokeStyle = "#916e0c"
+	c.fillStyle = "#916e0c"
+	c.ellipse(x, y, w, h, 0, 0 * Math.PI / 180,  180 * Math.PI / 180)
+	c.stroke()
+	c.fill()
+	//return x, y
+}
+
+//createBasket(350, 570)
+
+var canvasObject = {
+	"basket": [350, 570, 4, 60, 90],
+	"eggs": []
+}
+
+function movePlatform (event) {
+	let key = event.key
+	if (key === "ArrowRight") {//"right") {
+		canvasObject["basket"][0] += canvasObject["basket"][2]//1
+	} else if (key === "ArrowLeft") {//"left") {
+		canvasObject["basket"][0] -= canvasObject["basket"][2]//1
+	}
+	//console.log(event)
+	//if (key === "right") {
+	//	createBasket(canvasObject["basket"][0] + 1, canvasObject["basket"][0])
+	//} else if (key === "left") {
+	//	createBasket(canvasObject["basket"][0] - 1, canvasObject["basket"][0])
+}
+
+window.addEventListener("keydown", (event) => {movePlatform(event)})
+
+var time = 0
+var dy = 2
+var health = 5
+var points = 0
+var pointsIgnore = [0]
+
+function animation () {
+	c.beginPath()
+	c.clearRect(0, 0, w, 700, 700) //clearRect()//(0, 0, 700, 700)
+	//console.log("Clear", canvasObject["basket"])
+	drawBg()
+
+	canvasObject["eggs"].forEach((item, index) => {
+		createEgg(item[0], item[1], item[2], item[3], item[4])
+		if (item[1] + 30 > 700) {//canvas.width) {
+			canvasObject["eggs"].splice(index, 1)
+			//console.log(canvasObject["eggs"])
+			//console.log(item, canvasObject["eggs"].indexOf(item) === index)
+			--health //-= 1
+		}
+		
+		//canvasObject["basket"][0] - canvasObject["basket"][3] / 2 < item[0] < canvasObject["basket"][0] + canvasObject["basket"][3] / 2
+		
+		//console.log(canvasObject["basket"][0] - canvasObject["basket"][3] / 2, item[0], canvasObject["basket"][0] + canvasObject["basket"][3] / 2)
+		if ((canvasObject["basket"][0] - canvasObject["basket"][3] < item[0] &&
+		 item[0] < canvasObject["basket"][0] + canvasObject["basket"][3]) && (canvasObject["basket"][1] < item[1] + item[4])) {//(canvasObject["basket"][0] - canvasObject["basket"][3] < item[0] < canvasObject["basket"][0] + canvasObject["basket"][3]){//canvasObject["basket"][3] / 2 + item[0]) {
+			//console.log([tem[0] - canvasObject["eggs"][3] / 2, item[0], canvasObject["eggs"][3] / 2 + item[3] + item[0]])
+			console.log("COLLIDE")
+			++points
+			canvasObject["eggs"].splice(index, 1)
+		}
+
+		// console.log(canvasObject["basket"][0] - canvasObject["basket"][3] / 2 < item[0] < canvasObject["basket"][0] + canvasObject["basket"][3] / 2)
+
+		//console.log(canvasObject["basket"][0] - canvasObject["basket"][3], item[0], canvasObject["basket"][0] + canvasObject["basket"][3])//item, )
+		//console.log(item[0] - canvasObject["eggs"][3] / 2, item[0], canvasObject["eggs"][3] / 2 + item[3] + item[0])
+		//console.log(item)
+
+		item[1] += dy
+	})
+
+	c.beginPath()
+
+	createBasket(canvasObject["basket"][0], canvasObject["basket"][1], canvasObject["basket"][3], canvasObject["basket"][4])
+	// let key = event.key
+	// if (key === "right") {
+	// 	createBasket(canvasObject["basket"][0] + 1, canvasObject["basket"][0])
+	// } else if (key === "left") {
+	// 	createBasket(canvasObject["basket"][0] - 1, canvasObject["basket"][0])
+	// } else {
+	// 	createBasket(canvasObject["basket"][0], canvasObject["basket"][0])
+	// }
+
+	c.fillStyle = "#e3ae1b"
+	c.font = "60px Arial"
+	c.textAlign = 'left'
+
+	c.fillText(points, 30, 50)
+
+	c.fillStyle = "#ff2a00"
+	c.textAlign = 'right'
+	c.fillText(health, canvas.width - 30, 50)
+	//strokeStyle="#e3ae1b"
+	//c.stroke()
+
+	time += 1
+	if (time === 60 * 5) {
+		time = 0
+		let x = Math.random() * (680 - 20 + 1) + 20
+		let y = Math.random() * (300 - 20 + 1) + 20
+		let color = [Math.random() * (255 + 1), Math.random() * (255 + 1), Math.random() * (255 + 1)]
+		canvasObject["eggs"].push([x, y, color, 30, 50])
+		//createEgg()
+	}
+
+	//if //(points / 5 === 0 && points != 0) {
+	//	++dy
+	//}
+	//console.log(points / 5)
+	if ((points / 5 === 1) && !(pointsIgnore.includes(points))) {
+		console.log('FUNC')
+		dy += 1
+		pointsIgnore.push(points)
+		console.log('FUNC')
+	}
+
+	//console.log(dy)
+	//console.log(c.coords())
+
+	const tm = setTimeout(() => {requestAnimationFrame(animation)}, 16.6)
+
+	if (health === 0) {
+		clearTimeout(tm)	
+	}
+}
+
+animation()
+//createEgg(350, 250)
